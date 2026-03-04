5e01f6bce0a31a887fb349086b4671ea4d03e483
kpodwysocka <33017461+kpodwysocka@users.noreply.github.com>
2026-03-04 18:05:34 +0100

    Update schema.sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -34,3 +34,10 @@ CREATE TABLE order_items (
     FOREIGN KEY (product_id) 
         REFERENCES products(product_id)
 );
+-- Indexes for performance
+
+CREATE INDEX idx_orders_customer_id 
+ON orders(customer_id);
+
+CREATE INDEX idx_order_items_product_id 
+ON order_items(product_id);
