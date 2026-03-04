ca05180a9e3d3dd9ca6e9d4c99b9812e5bba1a10
kpodwysocka <33017461+kpodwysocka@users.noreply.github.com>
2026-03-04 18:00:17 +0100

    Create queries.sql

---
+++ sql/queries.sql
@@ -0,0 +1,14 @@
+--Total revenue per customer
+SELECT c.name, SUM(p.price * oi.quantity) AS total_spent
+FROM customers c
+JOIN orders o ON c.customer_id = o.customer_id
+JOIN order_items oi ON o.order_id = oi.order_id
+JOIN products p ON oi.product_id = p.product_id
+GROUP BY c.name;
+
+-- Top selling products
+SELECT p.product_name, SUM(oi.quantity) AS total_sold
+FROM products p
+JOIN order_items oi ON p.product_id = oi.product_id
+GROUP BY p.product_name
+ORDER BY total_sold DESC;
