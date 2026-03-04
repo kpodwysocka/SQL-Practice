dc9590827a9b07a797bab47573830828c393c524
kpodwysocka <33017461+kpodwysocka@users.noreply.github.com>
2026-03-04 18:04:17 +0100

    Update schema.sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,29 +1,36 @@
--- DDL
+-- DDL: Trying constraints
 
 CREATE TABLE customers (
     customer_id INT PRIMARY KEY,
     name TEXT NOT NULL,
-    city TEXT
+    city TEXT NOT NULL,
+    email TEXT UNIQUE
 );
 
 CREATE TABLE products (
     product_id INT PRIMARY KEY,
     product_name TEXT NOT NULL,
-    price NUMERIC NOT NULL
+    price NUMERIC NOT NULL CHECK (price > 0)
 );
 
 CREATE TABLE orders (
     order_id INT PRIMARY KEY,
-    customer_id INT,
-    order_date DATE,
-    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+    customer_id INT NOT NULL,
+    order_date DATE NOT NULL,
+    status TEXT DEFAULT 'NEW',
+    FOREIGN KEY (customer_id) 
+        REFERENCES customers(customer_id)
+        ON DELETE CASCADE
 );
 
 CREATE TABLE order_items (
     order_item_id INT PRIMARY KEY,
-    order_id INT,
-    product_id INT,
-    quantity INT,
-    FOREIGN KEY (order_id) REFERENCES orders(order_id),
-    FOREIGN KEY (product_id) REFERENCES products(product_id)
+    order_id INT NOT NULL,
+    product_id INT NOT NULL,
+    quantity INT NOT NULL CHECK (quantity > 0),
+    FOREIGN KEY (order_id) 
+        REFERENCES orders(order_id)
+        ON DELETE CASCADE,
+    FOREIGN KEY (product_id) 
+        REFERENCES products(product_id)
 );
